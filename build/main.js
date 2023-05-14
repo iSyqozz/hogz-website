5201ef5721f35108b15d234c8412f2bb65d810db
Fares Hassen <fares.hassen512@gmail.com>
2023-05-14 18:11:19 +0100

    test

--- build/main.js
+++ build/main.js
@@ -3,6 +3,7 @@ function main() {
     const lower_image = document.querySelector('.first-image');
     const logo_image = document.querySelector('.logo-image');
     const welcome = document.querySelector('.welcome');
+    const logo_top = document.querySelector('.logo-top');
     setTimeout(() => {
         lower_image.classList.add('first-image-appear');
     }, 100);
@@ -17,6 +18,9 @@ function main() {
         setTimeout(() => {
             logo_image.classList.add('logo-image-disappear');
         }, 1300);
+        setTimeout(() => {
+            logo_top.classList.add('logo-top-appear');
+        }, 2000);
     }, 2100);
 }
 window.onload = () => {
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ function main(){
     const lower_image = document.querySelector('.first-image') as HTMLElement;
     const logo_image = document.querySelector('.logo-image') as HTMLElement;
     const welcome = document.querySelector('.welcome') as HTMLElement;
+    const logo_top = document.querySelector('.logo-top') as HTMLElement;
 
     setTimeout(() => {
         lower_image.classList.add('first-image-appear');
@@ -25,6 +26,10 @@ function main(){
         setTimeout(() => {
             logo_image.classList.add('logo-image-disappear');
         }, 1300);
+
+        setTimeout(() => {
+            logo_top.classList.add('logo-top-appear');
+        }, 2000);
     }, 2100);
 
 }
