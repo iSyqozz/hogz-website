2e231a867fc6d5d6b12b754e8db935e720df2a8b
Fares Hassen <fares.hassen512@gmail.com>
2023-05-14 17:36:57 +0100

    test

--- build/main.js
+++ build/main.js
@@ -1,9 +1,23 @@
 "use strict";
 function main() {
-    const logo = document.querySelector('.mid-logo');
+    const lower_image = document.querySelector('.first-image');
+    const logo_image = document.querySelector('.logo-image');
+    const welcome = document.querySelector('.welcome');
     setTimeout(() => {
-        logo.classList.add('float-up');
-    }, 1500);
+        lower_image.classList.add('first-image-appear');
+    }, 100);
+    setTimeout(() => {
+        logo_image.classList.add('logo-image-appear');
+    }, 100);
+    setTimeout(() => {
+        welcome.classList.add('welcome-appear');
+        setTimeout(() => {
+            welcome.style.animation = 'float 1.5s ease-in-out infinite';
+        }, 1000);
+        setTimeout(() => {
+            logo_image.classList.add('logo-image-disappear');
+        }, 1300);
+    }, 2100);
 }
 window.onload = () => {
     main();
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,30 @@
 
 
 function main(){
-    const logo = document.querySelector('.mid-logo') as HTMLElement;
+    const lower_image = document.querySelector('.first-image') as HTMLElement;
+    const logo_image = document.querySelector('.logo-image') as HTMLElement;
+    const welcome = document.querySelector('.welcome') as HTMLElement;
 
     setTimeout(() => {
+        lower_image.classList.add('first-image-appear');
+    }, 100);
 
-        logo.classList.add('float-up');
-        
-    }, 1500);
+    setTimeout(() => {
+        logo_image.classList.add('logo-image-appear');
+    }, 100);
 
+    setTimeout(() => {
+        welcome.classList.add('welcome-appear');
+        setTimeout(() => {
 
+            welcome.style.animation = 'float 1.5s ease-in-out infinite';
+            
+        }, 1000);
+
+        setTimeout(() => {
+            logo_image.classList.add('logo-image-disappear');
+        }, 1300);
+    }, 2100);
 
 }
 
